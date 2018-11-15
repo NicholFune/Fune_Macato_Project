7ef94495aa64a63ce148d6cee589411c3f6f6c91
NicholFune <2015081045@ust-ics.mygbiz.com>
2018-11-15 22:48:20 +0800

    Initial commit

---
+++ app/src/main/java/com/fune/macato/Equipment.java
@@ -0,0 +1,13 @@
+package com.fune.macato;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Equipment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_equipment2);
+    }
+}
---
+++ app/src/main/java/com/fune/macato/Menu.java
@@ -0,0 +1,36 @@
+package com.fune.macato;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+
+    public void process(View v) {
+        Intent i = null, chooser = null;
+        if (v.getId() == R.id.button) {
+            i = new Intent(this, HowToFish.class);
+            startActivity(i);
+        } else if (v.getId() == R.id.button3) {
+            i = new Intent(this, Equipment.class);
+            startActivity(i);
+        } else if (v.getId() == R.id.button2) {
+            i = new Intent(this, FishingEtiquette.class);
+            startActivity(i);
+        } else if (v.getId() == R.id.button4) {
+            i = new Intent(this, UnsafeFishing.class);
+            startActivity(i);
+        } else if (v.getId() == R.id.button5) {
+            i = new Intent(this, AboutUs.class);
+            startActivity(i);
+        }
+    }
+}
